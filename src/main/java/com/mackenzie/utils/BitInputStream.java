70da199d8941dc02578d264f37b144c0518c8117
wvxbs <gabriel.ferreira7854@gmail.com>
2025-09-25 15:15:14 -0300

    Helpers de stream de bits adicionados

---
+++ src/main/java/com/mackenzie/utils/BitInputStream.java
@@ -0,0 +1,33 @@
+package com.mackenzie.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class BitInputStream implements AutoCloseable {
+    private final InputStream in;
+    private int buffer;
+    private int count;
+
+    public BitInputStream(InputStream in) {
+        this.in = in;
+        this.count = 0;
+    }
+
+    public int readBit() throws IOException {
+        if (count == 0) {
+            buffer = in.read();
+            if (buffer == -1) {
+                return -1;
+            }
+            count = 8;
+        }
+        count--;
+
+        return (buffer >> count) & 1;
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+    }
+}
---
+++ src/main/java/com/mackenzie/utils/BitOutputStream.java
@@ -0,0 +1,33 @@
+package com.mackenzie.utils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class BitOutputStream implements AutoCloseable {
+    private final OutputStream out;
+    private int buffer;
+    private int count;
+
+    public BitOutputStream(OutputStream out) {
+        this.out = out;
+    }
+
+    public void writeBit(int bit) throws IOException {
+        buffer = (buffer << 1) | bit;
+        count++;
+        if (count == 8) {
+            out.write(buffer);
+            count = 0;
+            buffer = 0;
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (count > 0) {
+
+            out.write(buffer << (8 - count));
+        }
+        out.close();
+    }
+}
